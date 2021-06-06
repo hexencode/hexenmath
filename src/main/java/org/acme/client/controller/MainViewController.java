a42b6c757190aeb58e0f4c4285fc8186b8d9d64c
j.kotek <j.kotek@scopegroup.com>
2021-06-06 07:28:50 +0200

    Adding search button

--- src/main/java/org/acme/client/controller/MainViewController.java
+++ src/main/java/org/acme/client/controller/MainViewController.java
@@ -3,12 +3,14 @@ package org.acme.client.controller;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
 import org.acme.client.model.FxOrder;
 import org.acme.server.model.Order;
 import org.acme.server.service.IOrderService;
@@ -18,8 +20,8 @@ import javax.inject.Singleton;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.stream.Collectors;
 
+@Slf4j
 @Singleton
 public class MainViewController implements Initializable {
 
@@ -27,6 +29,8 @@ public class MainViewController implements Initializable {
     public TableColumn<FxOrder, Integer> orderIdColumn;
     public TableColumn<FxOrder, String> stateColumn;
     public TableColumn<FxOrder, String> cityColumn;
+
+    private final ObservableList<FxOrder> orderObservableList = FXCollections.observableArrayList();
 
     @Inject
     IOrderService orderService;
@@ -36,14 +40,7 @@ public class MainViewController implements Initializable {
         orderIdColumn.setCellValueFactory(cellData -> cellData.getValue().idProperty().asObject());
         stateColumn.setCellValueFactory(cellData -> cellData.getValue().stateProperty());
         cityColumn.setCellValueFactory(cellData -> cellData.getValue().cityProperty());
-
-        List<Order> orders = orderService.getOrders();
-
-        exampleTable.setItems(FXCollections.observableList(orders.stream()
-                        .map(FxOrder::new)
-                        .collect(Collectors.toList())
-                )
-        );
+        exampleTable.setItems(orderObservableList);
     }
 
     @FXML
@@ -60,5 +57,14 @@ public class MainViewController implements Initializable {
             }
         }.getHostServices().showDocument("https://github.com/hexencode/hexenmath");
         event.consume();
+    }
+
+    @FXML
+    public void handleSearchButtonClicked(ActionEvent event) {
+        log.info("Searching for orders...");
+        List<Order> orders = orderService.getOrders();
+        orders.stream()
+                .map(FxOrder::new)
+                .forEach(orderObservableList::add);
     }
 }
